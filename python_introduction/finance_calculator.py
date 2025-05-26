90753cabb61735e1aaf9cb02caff0d49400d39e1
KELVIN <kelvinkipyegon568@gmail.com>
2025-05-26 22:43:36 +0300

    corrected user input at the monthly_income variable

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,4 +1,4 @@
-monthly_income = int(input("Enter your monthly savings: "))
+monthly_income = int(input("Enter your monthly income: "))
 monthly_expenses = int(input("Enter your monthly expenses: "))
 
 monthly_savings = monthly_income -monthly_expenses
