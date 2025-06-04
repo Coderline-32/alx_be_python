97c058b2a942c797d718ad7b67c0040015ed764f
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-04 17:34:17 +0300

    changer operator variable to operation

--- match_case_calculator.py
+++ match_case_calculator.py
@@ -1,8 +1,8 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operator = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operator:
+match operation:
     case "+":
         output = num1 + num2
         print(f"The result is {output}")
