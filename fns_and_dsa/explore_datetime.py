8488d5072abe037e32ae2822e79300b7df255c1d
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-07 15:49:26 +0300

    changed user input on number of days to exact

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
     print(f"Current date and time: {formatted}")
 
 def calculate_future_date():
-    days_entry = int(input("Enter number of days: "))
+    days_entry = int(input("Enter the number of days to add to the current date: "))
     future_date = current_date + timedelta(days=days_entry)
     formatted_future = future_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"The date in future will be {formatted_future}")
