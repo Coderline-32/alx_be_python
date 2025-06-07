278bcbd71d285be8b572567533ae46b66503ff3f
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-07 15:44:57 +0300

    Corrected import statement adding from

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,17 +1,14 @@
-import datetime
-
-
-
+from datetime import datetime, timedelta
 
 
 def display_current_datetime():
     global current_date
-    current_date =  datetime.datetime.now()
+    current_date =  datetime.now()
     
 
 def calculate_future_date():
     days_entry = int(input("Enter number of days: "))
-    future_date  = current_date + datetime.timedelta(days = days_entry)
+    future_date  = current_date + timedelta(days = days_entry)
     print(f"The date in future will be {future_date}") 
 display_current_datetime()
 calculate_future_date()
