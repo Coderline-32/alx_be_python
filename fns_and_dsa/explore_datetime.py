860875214be454393cd111716d419172e92ff68f
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-07 15:47:21 +0300

    Updated the date and time format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,14 +1,16 @@
 from datetime import datetime, timedelta
 
-
 def display_current_datetime():
     global current_date
-    current_date =  datetime.now()
-    
+    current_date = datetime.now()
+    formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted}")
 
 def calculate_future_date():
     days_entry = int(input("Enter number of days: "))
-    future_date  = current_date + timedelta(days = days_entry)
-    print(f"The date in future will be {future_date}") 
+    future_date = current_date + timedelta(days=days_entry)
+    formatted_future = future_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"The date in future will be {formatted_future}")
+
 display_current_datetime()
 calculate_future_date()
