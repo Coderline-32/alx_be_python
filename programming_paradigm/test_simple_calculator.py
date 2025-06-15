a950d39c1ce6d3e1d55101d46a067a6ab8bcc30c
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-15 17:21:12 +0300

    Add unit tests for all basic arithmetic methods in SimpleCalculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,23 +6,23 @@ class TestSimpleCalculator(unittest.TestCase):
         # Set up the simple calculator instance before each test.
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2,3), 5)
         self.assertEqual(self.calc.add(-2,-2), -4)
         self.assertEqual(self.calc.add(-2,2), 0)
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,3), -1)
         self.assertEqual(self.calc.subtract(-2,-2), 0)
         self.assertEqual(self.calc.subtract(-2,2), -4)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,3), 6)
         self.assertEqual(self.calc.multiply(-2,-2), 4)
         self.assertEqual(self.calc.multiply(-2,2), -4)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(6,2), 3)
         self.assertEqual(self.calc.divide(-2,-2), 1)
         self.assertEqual(self.calc.divide(-2,2), -1)
-        self.assertEqual(self.calc.divide(-2,0), None)
+        self.assertIsNone(self.calc.divide(-2,0))
 
 
 
