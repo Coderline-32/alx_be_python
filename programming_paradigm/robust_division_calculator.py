391d20e389304fede9c3e098c2f3a26d45b49b48
KELVIN <kelvinkipyegon568@gmail.com>
2025-06-15 14:26:27 +0300

    Created main file and calculator file

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        result = num / den
+        return f"Result: {result}"
+    except ZeroDivisionError:
+        print("Cannot divide by Zero!")
+    
+    except ValueError:
+        print("Error: please enter numeric inputs")
